d1c1bf98db8f653f95c0069dab73e8fc1102d7da
BJanssen78 <98946596+BJanssen78@users.noreply.github.com>
2022-08-30 21:05:47 +0200

    Started writing new game

--- script/guessTheNr.js
+++ script/guessTheNr.js
@@ -1,13 +1,24 @@
 //start variable
 let userLives = 5;
-let userAttempt = 5;
-let userMaxNr = 25;
+// let userAttempt = 5;
+let userMaxNr = 25; // Optioneel door gebruiker in te voeren
+let userName = String('')
 
 //seperate functies
 const createNr = function(){
     let randomnr = Math.floor(Math.random() * userMaxNr);
     console.log(randomnr);
     return randomnr;
+}
+
+const greeting = function(userName){
+    if(userName == ''){
+        let userName = prompt(String('Welcome, may i know your name'));
+        return userName;
+    }
+    else{
+        alert('Welcome ' + userName);
+    }
 }
 
 const userAttemptCheck = function (){
@@ -19,11 +30,46 @@ const userAttemptCheck = function (){
             }
             else{
                 //exit the game
+                return alert('Good bye, see you next time')
             }
     }
     else{
         //continue the game
     }
 }
+
 //execution block
 createNr();
+greeting('');
+
+const numberGame = function(){
+    for (let userAttempt = 5; userAttempt == 0;){
+    createNr();
+    greeting('');
+    let userAnswer = prompt('Would you like to guess the number i have in mind?' + 
+                            'It\'s very simpel, just type a number between 0 and '+ maxNumber +
+                            'If your answer is correct, you get an additional live, if your answer is wrong, you lose a live' +
+                            'You have 5 attempts to guess the number' +
+                            'If you don\'t want to play or stop the game, just type stop');
+    if(userAnswer == 'stop' || 'Stop'){
+        return alert('Sorry to see you leave, see you next time'), userAttempt - 5;
+    }
+    else if(userAnswer === randomnr){
+        userlives++;
+        alert('Yeah, well done, here\'s your reward.' +
+                'You\'re new live count is ') + userlives; 
+        createNr();
+        return;
+    }
+    else{
+        let userHintMin = randomnr - 5;
+        let userHintMax = randomnr + 5;
+        userAttempt--;
+        userlives--;
+        prompt('Sorry, that is not correct' + 
+            'The number is between ' + userHintMin + ' and ' + userHintMax +
+            'You have ' + userAttempt + ' attempts and ' + userlives + ' lives left.');
+        return;
+    }
+}
+}
---
+++ script/repl.js
@@ -0,0 +1,4 @@
+for (let i = 5; i == 10; i++){
+    // i - 5;
+    console.log(i);
+  }
